6258430036f33d244be09374702d1d79bb1d1f22
dilshod <seferonxkiller@>
2022-11-14 14:07:11 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+
+
+TOKEN = '5433369467:AAE-sL7wXcoiP7fXbSlbx7bY8gZvGNkJBwU'
+
+
+
+STARTMSG = "Men Tarjimon botman👋 "
+CHOOSEMSG = "Matnni qaysi tilga tarjima qilmoqchisiz?"
+
+
+LANGDICT = {
+
+    'uz': '🇺🇿Uzbek',
+    'nl': '🇳🇱Dutch',
+    'en': '🇺🇸English',
+    'fr': '🇫🇷French',
+    'ko': '🇰🇷Korean',
+    'ru': '🇷🇺Russian',
+
+  }
+LANGUES = LANGDICT.keys()
